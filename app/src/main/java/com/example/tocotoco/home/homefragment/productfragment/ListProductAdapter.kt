0ca7f02fd531f4869d10e8eb9d1293c89a427cd7
Hoang Manh Cuong <cuonghm21022002@gmail.com>
2022-11-23 22:03:34 +0700

    add intent detail

--- app/src/main/java/com/example/tocotoco/home/homefragment/productfragment/ListProductAdapter.kt
+++ app/src/main/java/com/example/tocotoco/home/homefragment/productfragment/ListProductAdapter.kt
@@ -11,7 +11,9 @@ import com.example.tocotoco.basekotlin.extensions.decimalFormatted
 import com.example.tocotoco.databinding.ItemListTeaBinding
 import com.example.tocotoco.model.ProductsByCategoryResultModel
 
-class ListProductAdapter :
+class ListProductAdapter(
+    private val onItemClick: (Int) -> Unit
+) :
     ListAdapter<ProductsByCategoryResultModel, ListProductAdapter.ViewHolder>(
         object : DiffUtil.ItemCallback<ProductsByCategoryResultModel>() {
             override fun areItemsTheSame(
@@ -42,9 +44,12 @@ class ListProductAdapter :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
+        holder.binding.root.setOnClickListener {
+            onItemClick(getItem(position).id)
+        }
     }
 
-    class ViewHolder(private val binding: ItemListTeaBinding) :
+    class ViewHolder(val binding: ItemListTeaBinding) :
         RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
         fun bind(item: ProductsByCategoryResultModel) = binding.run {
--- app/src/main/java/com/example/tocotoco/home/homefragment/productfragment/ListProductFragment.kt
+++ app/src/main/java/com/example/tocotoco/home/homefragment/productfragment/ListProductFragment.kt
@@ -1,5 +1,6 @@
 package com.example.tocotoco.home.homefragment.productfragment
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.tocotoco.R
 import com.example.tocotoco.basekotlin.base.BaseFragment
@@ -7,6 +8,7 @@ import com.example.tocotoco.basekotlin.base.BaseViewModel
 import com.example.tocotoco.basekotlin.extensions.viewBinding
 import com.example.tocotoco.databinding.FragmentListProductBinding
 import com.example.tocotoco.dialog.DialogUtils
+import com.example.tocotoco.feature.product_detail.ProductDetailActivity
 import com.example.tocotoco.model.ProductsByCategoryResult
 import com.example.tocotoco.network.NetWorkController
 import com.example.tocotoco.network.TCCCallback
@@ -29,7 +31,11 @@ class ListProductFragment : BaseFragment(R.layout.fragment_list_product) {
     }
 
     private val listProductAdapter by lazy(LazyThreadSafetyMode.NONE) {
-        ListProductAdapter()
+        ListProductAdapter(onItemClick = {
+            val intent = Intent(requireActivity(), ProductDetailActivity::class.java)
+            intent.putExtra("idProduct", it)
+            startActivity(intent)
+        })
     }
 
     override fun setupViews() {
