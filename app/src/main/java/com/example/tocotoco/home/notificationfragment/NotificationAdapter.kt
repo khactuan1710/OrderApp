24edaa8f93e367a744ea9b1e5aefe381d294520b
Hoang Manh Cuong <cuonghm21022002@gmail.com>
2022-12-07 22:11:04 +0700

    call api notification

--- app/src/main/java/com/example/tocotoco/home/notificationfragment/NotificationAdapter.kt
+++ app/src/main/java/com/example/tocotoco/home/notificationfragment/NotificationAdapter.kt
@@ -5,23 +5,24 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tocotoco.databinding.ItemNotificationBinding
-import com.example.tocotoco.model.ProductsResult.ProductsResultModel
+import com.example.tocotoco.model.NotifiResult.NotifiModel
 
 class NotificationAdapter(
     private val onItemClick: (Int) -> Unit
-) : ListAdapter<ProductsResultModel, NotificationAdapter.ViewHolder>(
-    object : DiffUtil.ItemCallback<ProductsResultModel>() {
+) : ListAdapter<NotifiModel, NotificationAdapter.ViewHolder>(
+    object : DiffUtil.ItemCallback<NotifiModel>() {
         override fun areItemsTheSame(
-            oldItem: ProductsResultModel,
-            newItem: ProductsResultModel
+            oldItem: NotifiModel,
+            newItem: NotifiModel
         ): Boolean {
             return oldItem == newItem
         }
 
         override fun areContentsTheSame(
-            oldItem: ProductsResultModel,
-            newItem: ProductsResultModel
+            oldItem: NotifiModel,
+            newItem: NotifiModel
         ): Boolean {
             return oldItem.id == newItem.id
         }
@@ -48,8 +49,14 @@ class NotificationAdapter(
     }
 
     class ViewHolder(val binding: ItemNotificationBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: ProductsResultModel) = binding.run {
-
+        fun bind(item: NotifiModel) = binding.run {
+            tvNotification.text = item.title
+            Glide
+                .with(root.context)
+                .load(item.image)
+                .centerCrop()
+                .dontAnimate()
+                .into(imgNotification)
         }
     }
 }
--- app/src/main/java/com/example/tocotoco/home/notificationfragment/NotificationFragment.kt
+++ app/src/main/java/com/example/tocotoco/home/notificationfragment/NotificationFragment.kt
@@ -1,12 +1,22 @@
 package com.example.tocotoco.home.notificationfragment
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import androidx.core.view.isVisible
 import com.example.tocotoco.R
 import com.example.tocotoco.basekotlin.base.BaseFragment
 import com.example.tocotoco.basekotlin.base.BaseViewModel
 import com.example.tocotoco.basekotlin.extensions.viewBinding
 import com.example.tocotoco.databinding.FragmentNotificationBinding
+import com.example.tocotoco.dialog.DialogUtils
 import com.example.tocotoco.feature.product_detail.ProductDetailActivity
+import com.example.tocotoco.model.NotifiResult
+import com.example.tocotoco.network.NetWorkController
+import com.example.tocotoco.network.TCCCallback
+import com.example.tocotoco.util.NetworkUtils
+import retrofit2.Call
+import retrofit2.Response
+import timber.log.Timber
 
 class NotificationFragment : BaseFragment(R.layout.fragment_notification){
     override val binding: FragmentNotificationBinding by viewBinding()
@@ -26,12 +36,59 @@ class NotificationFragment : BaseFragment(R.layout.fragment_notification){
             startActivity(intent)
         })
     }
+
     override val viewModel: BaseViewModel
         get() = TODO("Not yet implemented")
 
     override fun setupViews() {
         setupRecyclerView()
+        getListNotification()
     }
+
+
+    private fun getListNotification() {
+        DialogUtils.showProgressDialog(requireActivity())
+        if (NetworkUtils.isConnect(requireActivity())) {
+            NetWorkController.getPromoteNotifications(object :
+                TCCCallback<NotifiResult>() {
+                @SuppressLint("SetTextI18n")
+                override fun onTCTCSuccess(
+                    call: Call<NotifiResult>?,
+                    response: Response<NotifiResult>?
+                ) {
+                    if (response?.body()?.result != null) {
+                        promoteAdapter.submitList(response.body()?.result)
+                    }
+                }
+
+                override fun onTCTCFailure(call: Call<NotifiResult>?) {
+                    Timber.tag(call.toString())
+                    DialogUtils.dismissProgressDialog()
+                }
+            })
+            NetWorkController.getNewsNotifications(object :
+                TCCCallback<NotifiResult>() {
+                @SuppressLint("SetTextI18n")
+                override fun onTCTCSuccess(
+                    call: Call<NotifiResult>?,
+                    response: Response<NotifiResult>?
+                ) {
+                    if (response?.body()?.result != null) {
+                        newsAdapter.submitList(response.body()?.result)
+                    }
+
+                }
+
+                override fun onTCTCFailure(call: Call<NotifiResult>?) {
+                    Timber.tag(call.toString())
+                    DialogUtils.dismissProgressDialog()
+                }
+            })
+            DialogUtils.dismissProgressDialog()
+            binding.root.isVisible = true
+        }
+    }
+
 
     private fun setupRecyclerView() = binding.run {
         rvKhuyenMai.adapter = promoteAdapter
