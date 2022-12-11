5db18c98f95ef71ee4b6bf047f339b4330f4932d
Hoang Manh Cuong <cuonghm21022002@gmail.com>
2022-12-11 21:04:13 +0700

    add screen search

---
+++ app/src/main/java/com/example/tocotoco/home/search/SearchFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tocotoco.home.search
+
+import com.example.tocotoco.R
+import com.example.tocotoco.basekotlin.base.BaseFragment
+import com.example.tocotoco.basekotlin.base.BaseViewModel
+import com.example.tocotoco.basekotlin.extensions.viewBinding
+import com.example.tocotoco.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment(R.layout.fragment_search) {
+    override val binding: FragmentSearchBinding by viewBinding()
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun setupViews() {
+        //No TODO here
+    }
+
+    override fun bindViewModel() {
+        //No TODO here
+    }
+
+}
